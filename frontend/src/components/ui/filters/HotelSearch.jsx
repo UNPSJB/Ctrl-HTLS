aea8bc7c420b7e28ef9cd8af63c8d3a2ddd27151
Adriel-M-A <adriel.dev97@gmail.com>
2025-01-07 16:07:39 -0300

    Actualizar el diseño del componente HotelSearch para utilizar una cuadrícula; ajustar clases de estilo para mejorar la disposición de los elementos.

--- frontend/src/components/ui/filters/HotelSearch.jsx
+++ frontend/src/components/ui/filters/HotelSearch.jsx
@@ -1,25 +1,23 @@
-import React from 'react';
-
 const HotelSearch = () => {
   return (
-    <div className="flex flex-col md:flex-row items-center gap-4 border p-4 rounded-lg shadow-md">
+    <div className="grid grid-cols-1 md:grid-cols-7 gap-4 border p-4 rounded-lg shadow-md">
       {/* Input para ubicación/nombre del hotel */}
       <input
         type="text"
         placeholder="Ubicación o nombre del hotel"
-        className="flex-1 px-4 py-2 border border-primary-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary-500"
+        className="col-span-7 md:col-span-3 px-4 py-2 border border-primary-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary-500"
       />
 
       {/* Input para fecha de inicio */}
       <input
         type="date"
-        className="flex-1 px-4 py-2 border border-primary-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary-500"
+        className="col-span-7 md:col-span-1 px-4 py-2 border border-primary-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary-500"
       />
 
       {/* Input para fecha de fin */}
       <input
         type="date"
-        className="flex-1 px-4 py-2 border border-primary-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary-500"
+        className="col-span-7 md:col-span-1 px-4 py-2 border border-primary-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary-500"
       />
 
       {/* Input para cantidad de personas */}
@@ -27,11 +25,11 @@ const HotelSearch = () => {
         type="number"
         min="1"
         placeholder="Personas"
-        className="flex-1 px-4 py-2 border border-primary-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary-500"
+        className="col-span-7 md:col-span-1 px-4 py-2 border border-primary-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary-500"
       />
 
       {/* Botón de búsqueda */}
-      <button className="px-6 py-2 bg-primary-700 text-white font-semibold rounded-lg hover:bg-primary-900 transition focus:outline-none focus:ring-2 focus:ring-primary-500">
+      <button className="col-span-7 md:col-span-1 px-6 py-2 bg-primary-700 text-white font-semibold rounded-lg hover:bg-primary-900 transition focus:outline-none focus:ring-2 focus:ring-primary-500 md:self-center">
         Buscar
       </button>
     </div>
