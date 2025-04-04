3980b4a46090df819fa90b190373e80509702d73
Adriel-M-A <adriel.dev97@gmail.com>
2025-04-04 11:13:40 -0300

    Ajustar el espaciado en HotelHeader y agregar descripción del hotel

--- frontend/src/components/HotelHeader.jsx
+++ frontend/src/components/HotelHeader.jsx
@@ -27,7 +27,7 @@ const HotelHeader = ({
         />
       )}
 
-      <div className="flex flex-col flex-1 py-2 gap-2">
+      <div className="flex flex-col flex-1 py-2 gap-3">
         <h2 className="text-2xl font-bold text-gray-800 dark:text-gray-200">
           {hotel.nombre}
         </h2>
@@ -55,6 +55,10 @@ const HotelHeader = ({
           <MapPin className="w-5 h-5 text-gray-500 dark:text-gray-400" />
           {hotel.ubicacion.ciudad}, {hotel.ubicacion.provincia},{' '}
           {hotel.ubicacion.pais}
+        </p>
+
+        <p className=" text-gray-600 dark:text-gray-400 line-clamp-2">
+          {hotel.descripcion}
         </p>
       </div>
     </div>
