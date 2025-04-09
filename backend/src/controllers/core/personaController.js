2c68379f8cfcc9afed5014ab2c10da5a3005dc05
cachi1997 <cachigaliano1997@gmail.com>
2025-04-09 19:23:37 -0300

    Renombrar parámetro en getClienteByDocumento para mejorar claridad y consistencia en la función.

--- backend/src/controllers/core/personaController.js
+++ backend/src/controllers/core/personaController.js
@@ -154,11 +154,10 @@ const getClienteByDocumento = async (req, res) => {
   // if (!errors.isEmpty()) {
   //   return res.status(400).json({ errors: errors.array() });
   // }
-  const { numeroDocumento } = req.params;
+  const { documento } = req.params;
 
   try {
-    const cliente =
-      await personaServices.obtenerClientePorDocumento(numeroDocumento);
+    const cliente = await personaServices.obtenerClientePorDocumento(documento);
     return res.status(200).json(cliente);
   } catch (error) {
     const statusCode = error.statusCode || 500;
