0f98319da1d4f67cee1f7f4702940c4c21830514
Cachi <cachigaliano1997@gmail.com>
2026-05-06 20:12:44 -0300

    updt: ruta individual de busqueda de venta

--- backend/src/routes/ventas/facturasRoutes.js
+++ backend/src/routes/ventas/facturasRoutes.js
@@ -4,6 +4,7 @@ const {
   getVentasResumen,
   getFacturaPDF,
   buscarVentas,
+  getDetalleFactura,
 } = require('../../controllers/ventas/facturasController');
 const router = express.Router();
 
@@ -16,5 +17,7 @@ router.get('/ventas', getVentasResumen); // Resumen de ventas del día, semana y
 router.get('/factura/:id/pdf', getFacturaPDF); // Previsualizar PDF de una factura
 
 router.get('/buscar-ventas', buscarVentas); // Buscar ventas por filtros
+
+router.get('/factura/:id/detalle', getDetalleFactura); // Detalle completo de una factura
 
 module.exports = router;
